54b22a54990eb02eaa228712ef954bb20aa2f763
InakiAbete-Reflektion <inaki.abete@reflektion.com>
2018-10-27 20:07:29 -0300

    Add initial work for network tracker
    It only tracks `fetch` made requests

---
+++ src/tracker/network.js
@@ -0,0 +1,116 @@
+const originalFetch = window.fetch;
+
+function buildHarHeaders (headers) {
+  return headers ? Object.keys(headers).map(function (key) {
+    return {
+      name: key,
+      value: headers[key].toString()
+    };
+  }) : [];
+}
+
+function requestMethodProperties(method, request) {
+  switch (method) {
+    case 'POST':
+      return {
+        postData: request.body,
+      };
+    default:
+      return {};
+  }
+}
+
+const HAR = [];
+
+const interceptedFetch = (url, fetchOptions = {}) => {
+  const { method: requestMethod = 'GET' } = fetchOptions;
+
+  const request = {
+    url,
+    ...fetchOptions,
+    method: requestMethod,
+  };
+
+  const startTime = Date.now();
+  const timingStart = performance.now();
+
+  return originalFetch(url, fetchOptions)
+    .catch((e) => {
+      return Promise.reject(e);
+    })
+    .then((response) => {
+      const timingStop = performance.now();
+      const time = timingStop - timingStart;
+
+      const promisedResponse = response.clone();
+
+      response.text().then((body) => {
+        const entry = {
+          startedDateTime: new Date(startTime).toISOString(),
+          time,
+          request: {
+            method: requestMethod,
+            url: response.url,
+            cookies: [],
+            headers: buildHarHeaders(request.headers),
+            httpVersion: 'HTTP/2.0',
+            queryString: [],
+            headersSize: -1,
+            bodySize: -1,
+            ...requestMethodProperties(requestMethod, fetchOptions),
+          },
+          response: {
+            status: response.status,
+            statusText: response.statusText,
+            httpVersion: 'HTTP/2.0',
+            cookies: [],
+            headers: buildHarHeaders(response.headers),
+            _transferSize: body.length,
+            content: {
+              size: body.length,
+              mimeType: response.headers['content-type'],
+              text: body,
+            },
+            redirectURL: '',
+            headersSize: -1,
+            bodySize: -1
+          },
+          cache: {}
+        };
+
+        HAR.push(entry);
+        console.log('network', HAR);
+      });
+
+      return promisedResponse;
+    })
+};
+
+export default {
+  start() {
+    global.fetch = interceptedFetch;
+  },
+
+  stop() {
+    global.fetch = originalFetch;
+  },
+
+  get() {
+    const [{ startedDateTime } = {}] = HAR;
+    const har = {
+      log: {
+        version: '0.1',
+        creator: { name: 'alfredo-network-tracker', version: '0.1' },
+        pages: [{
+          startedDateTime,
+          id: 'alfredo-network-tracker',
+          title: 'alfredo-network-tracker',
+          pageTimings: { }
+        }],
+        entries: HAR,
+      }
+    };
+
+    return har;
+  }
+};
